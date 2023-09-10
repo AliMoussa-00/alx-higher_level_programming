e4526a73e28055303954d0adf5fafe0ebe7ec4f0
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-09-10 11:46:25 -0700

    task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,77 @@
+#include "lists.h"
+
+/**
+ * dup_list - duplicate a linked list.
+ * @head: addredd to the head of the list.
+ * Return: a new linked list.
+ */
+listint_t *dup_list(listint_t *head)
+{
+	listint_t *new = NULL;
+
+	while (head)
+	{
+		if (!add_nodeint_end(&new, head->n))
+			return (NULL);
+		head = head->next;
+	}
+
+	return (new);
+}
+
+/**
+ * reverse_list - reverse a linked list.
+ * @head: head of the linked list.
+ * Return: void.
+ */
+void reverse_list(listint_t **head)
+{
+	listint_t *current, *prev, *next;
+
+	if (!*head)
+		return;
+
+	prev = NULL;
+	current = *head;
+	next = *head;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+}
+
+/**
+ * is_palindrome - check if a linked list is palindrom.
+ * @head: heade of the linked list.
+ * Return: return 1 if it is palindrom else 0.
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *rev, *tmp;
+
+	if (!*head)
+		return (1);
+
+	tmp = *head;
+
+	rev = dup_list(*head);
+
+	reverse_list(&rev);
+
+	while (tmp && rev)
+	{
+		if (tmp->n != rev->n)
+			return (0);
+		tmp = tmp->next;
+		rev = rev->next;
+	}
+
+	free_listint(rev);
+	return (1);
+}
