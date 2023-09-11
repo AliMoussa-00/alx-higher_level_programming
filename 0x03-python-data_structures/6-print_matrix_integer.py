c56b9b7aa2717880c2c9423fd4fbac09a2b2543c
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-09-11 06:35:48 -0700

    fix 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -8,7 +8,7 @@ def print_matrix_integer(matrix=[[]]):
 
             for c in row:
                 if c != row[- 1]:
-                    print("{}".format(c), end=" ")
+                    print("{:d}".format(c), end=" ")
                 else:
-                    print("{}".format(c), end="")
+                    print("{:d}".format(c), end="")
             print()
