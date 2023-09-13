45af1d276a69fb8576ab73c29c65d7983e4cb746
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-09-13 13:29:54 -0700

    fix 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,6 +4,8 @@ def roman_to_int(roman_string):
 
     if roman_string == 'None' or not isinstance(roman_string, str):
         return 0
+    if len(roman_string) == 0:
+        return 0
 
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                   'C': 100, 'D': 500, 'M': 1000}
@@ -11,25 +13,16 @@ def roman_to_int(roman_string):
     string_dict = {}
     roman_int = 0
 
-    for roman in roman_string:
+    for i in range(len(roman_string)):
 
-        if roman not in roman_dict.keys():
+        if roman_string[i] not in roman_dict.keys():
             return 0
 
-        if roman in string_dict:
-            string_dict.update({roman: string_dict[roman] + roman_dict[roman]})
+        if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+
+            roman_int += roman_dict[roman_string[i]] * -1
         else:
-            string_dict.update({roman: roman_dict[roman]})
-
-    if max(string_dict, key=string_dict.get) == roman_string[0]:
-        roman_int = sum(string_dict.values())
-
-    elif max(string_dict, key=string_dict.get) == roman_string[-1]:
-        roman_int = list(reversed(string_dict.values()))[0]
-        for n in list(reversed(string_dict.values()))[1:]:
-            roman_int -= n
-
-    else:
-        roman_int = 0
+            roman_int += roman_dict[roman_string[i]]
 
     return roman_int
