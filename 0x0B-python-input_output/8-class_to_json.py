740b30fb467e97aa61dcadf7e3b2f8546e1bccd7
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-10-10 13:35:59 -0700

    tasks

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defining the '8-class_to_json' module"""
+
+import json
+
+
+def class_to_json(obj):
+    """ a function that returns the dictionary description
+    with simple data structure for JSON serialization of an object
+    """
+
+    return vars(obj)
