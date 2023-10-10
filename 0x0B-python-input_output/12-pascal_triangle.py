ca58b18ad0031e98a3d1cb660ba1fad8e1b3d0a9
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-10-10 15:08:52 -0700

    task .12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,4 +7,16 @@ def pascal_triangle(n):
     representing the Pascal’s triangle of n
     """
 
+    tri = [[1]]
 
+    for i in range(n):
+        t = tri[-1]
+        m = [1]
+
+        for j in range(len(t) - 1):
+            m.append(t[j] + t[j + 1])
+
+        m.append(1)
+        tri.append(m)
+
+    return tri
