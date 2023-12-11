22e5df12a5c360d09a6d233ad51f286ded0494b8
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-12-11 14:41:25 -0800

    fix ..5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,4 +4,4 @@ const argv = process.argv;
 
 const num = Math.floor(Number(process.argv[2]));
 
-if (isNaN(num) || typeof argv[2] === 'undefined') { console.log('Not a number'); } else { console.log(num); }
+console.log(isNaN(num) ? 'Not a number' : `'My number:' ${num}`)
