ad7b58516dc93e7bdc3def88a8cfa3856446424d
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-12-11 14:51:20 -0800

    fix 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,2 @@
 #!/usr/bin/node
-
-const argv = process.argv;
-
-let i = 0;
-
-while (typeof argv[i] !== 'undefined') {
-  if (i > 1) { console.log(argv[i]); }
-  i++;
-}
-
-if (i <= 2) { console.log('No argument'); }
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
