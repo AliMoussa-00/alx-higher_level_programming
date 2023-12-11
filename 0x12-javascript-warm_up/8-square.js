38e726478435593657a8ea6fd5dcddfb7097241f
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-12-11 14:45:40 -0800

    fix .8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
+const num = Math.floor(Number(process.argv[2]));
 
-const num = parseInt(argv[2]);
-
-if (isNaN(num) || typeof argv[2] === 'undefined') { console.log('Missing size'); } else {
+if (isNaN(num)) { console.log('Missing size'); } else {
   for (let i = 0; i < num; i++) {
     for (let j = 0; j < num; j++) { process.stdout.write('X'); }
     console.log();
