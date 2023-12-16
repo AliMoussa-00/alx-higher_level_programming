edeea44c41421669b8d267e74378208796b84f6a
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-12-16 12:47:47 -0800

    fix.9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -28,7 +28,5 @@ if __name__ == "__main__":
         if states:
             for state in states:
                 print(f"{state.id}: {state.name}")
-        else:
-            print("Nothing")
 
         session.close()
