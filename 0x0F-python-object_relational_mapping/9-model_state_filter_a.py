c36adba9206713de0e221e4f383b6200897c9db9
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-12-16 12:45:36 -0800

    fix .9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        states = session.query(State).filter(State.name.like('%a%')).all()
+        states = session.query(State).filter(State.name.contain('a'))
 
         if states:
             for state in states:
