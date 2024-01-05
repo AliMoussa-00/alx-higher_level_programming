0d9406ed0f8e8f09931651c8256344352fc9c5ae
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-01-05 10:37:33 -0800

    fix .101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a file with json content in POST request
-curl -X POST -H "Content-Type: application/json" -d "@$2" "$1"
+curl -sX POST -H "Content-Type: application/json" -d "@$2" "$1"
